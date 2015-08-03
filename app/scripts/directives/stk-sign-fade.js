2fb5010eba6d4c88688fd5859fee3d9f6ba3c09c
Ed Worboys <ed@audiu.net>
2015-08-03 17:10:53 +0100

    red/green animations

--- app/scripts/directives/stk-sign-fade.js
+++ app/scripts/directives/stk-sign-fade.js
@@ -1,18 +1,25 @@
 'use strict';
 
-/**
- * @ngdoc directive
- * @name stockDogApp.directive:stkSignFade
- * @description
- * # stkSignFade
- */
 angular.module('stockDogApp')
-  .directive('stkSignFade', function () {
+  .directive('stkSignFade', function ($animate) {
     return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the stkSignFade directive');
+      restrict: 'A',
+      link: function ($scope, $element, $attrs) {
+        var oldVal = null;
+        $attrs.$observe('stkSignFade', function (newVal) {
+          if (oldVal && oldVal == newVal) { return; }
+
+          var oldPrice = parseFloat(oldVal);
+          var newPrice = parseFloat(newVal);
+          oldVal = newVal;
+
+          if (oldPrice && newPrice) {
+            var direction = newPrice - oldPrice >= 0 ? 'up' : 'down';
+            $animate.addClass($element, 'change-' + direction, function() {
+              $animate.removeClass($element, 'change-' + direction);
+            });
+          }
+        });
       }
     };
   });
